6f95462fb4f1a229e611b89d5649872134b5011e
Mark Vallis <markvallis1987@Gmail.com>
2021-02-17 22:58:36 +0000

    Update docker_compose_logs.sh

--- Example Files and Notes/Bash Scripts/docker_compose_logs.sh
+++ Example Files and Notes/Bash Scripts/docker_compose_logs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Make sure you run this beforehand to enable execution "chmod +x dockerlogs.sh"
-# Usage: 
-#   - "./dockerlogs.sh uniquestring"
+# Usage: "./dockerlogs.sh uniquestring"
 #   - The "uniquestring" must be a completely unique part of the docker ps output else the "logs -f" command will fail
 
 readonly dockerProcesses=`docker ps`
@@ -11,7 +10,9 @@ if [ ! -z "$containerId" ] ; then
    docker logs -f $containerId
 fi
 
-# This can also be added as a function to the bash_profile/bash_rc:
+# This can also be added as a function to the bash_profile/bash_rc
+# Usage: "dclog uniquestring"
+#
 # dclog(){
 #   unset dockerProcesses
 #   unset containerId
