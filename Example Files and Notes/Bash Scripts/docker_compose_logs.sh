80245fd464285f79a414ef5372ff522278ccf67d
Mark Vallis <markvallis1987@Gmail.com>
2021-02-17 21:13:37 +0000

    Update docker_compose_logs.sh

--- Example Files and Notes/Bash Scripts/docker_compose_logs.sh
+++ Example Files and Notes/Bash Scripts/docker_compose_logs.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+# Make sure you run this beforehand to enable execution "chmod +x dockerlogs.sh"
+# Usage: 
+#   - "./dockerlogs.sh uniquestring"
+#   - The "uniquestring" must be a completely unique part of the docker ps output else the "logs -f" command will fail
 
-readonly imageName="scalacassandra:"
 readonly dockerProcesses=`docker ps`
-readonly containerId=$(echo "$dockerProcesses" | grep $imageName | awk '{print $1}')
+readonly containerId=$(echo "$dockerProcesses" | grep $1 | awk '{print $1}')
 
 if [ ! -z "$containerId" ] ; then
    docker logs -f $containerId
