0804cc570053e9ca69222c855a5a9a4679234ec9
Mark Vallis <markvallis1987@Gmail.com>
2025-05-01 15:25:22 +0100

    Update git_stale_branch_checker.sh

--- Example Files and Notes/Bash Scripts/git_stale_branch_checker.sh
+++ Example Files and Notes/Bash Scripts/git_stale_branch_checker.sh
@@ -5,9 +5,9 @@
 # Run using ./git_stale_branch_checker.sh
 
 # Example output:
-# Threshold date: Tue 1 Apr 2025 15:14:57 BST
-# Branch datastax_only is stale, the most recent commit was made on Mon 12 Jun 2023 18:54:01 BST
-# Branch master is stale, the most recent commit was made on Mon 28 Feb 2022 14:45:48 GMT
+# Threshold date: Tue 1 Apr 2025 15:24:35 BST
+# Branch datastax_only is stale, the most recent commit was made by mveeprojects on Mon 12 Jun 2023 18:54:01 BST
+# Branch master is stale, the most recent commit was made by mveeprojects on Mon 28 Feb 2022 14:45:48 GMT
 
 git fetch --prune
 
@@ -20,9 +20,10 @@ echo Threshold date: $(date -r $THRESHOLD_DATE)
 for branch in $(git branch -r | grep -v '\->'); do
     # Get the last commit date of the branch
     LAST_COMMIT_DATE=$(git log -1 --format=%ct "${branch#origin/}")
+    LAST_COMMIT_AUTHOR=$(git log -1 --pretty=format:%an "${branch#origin/}")
 
     # Check if the branch is older than the threshold
     if [[ $LAST_COMMIT_DATE -lt $THRESHOLD_DATE ]]; then
-             echo "Branch ${branch#origin/} is stale, the most recent commit was made on $(date -r $LAST_COMMIT_DATE)"
+             echo "Branch ${branch#origin/} is stale, the most recent commit was made by ${LAST_COMMIT_AUTHOR} on $(date -r $LAST_COMMIT_DATE)"
   fi
 done
