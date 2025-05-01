d0198c50d4b174f510e075af1c9139845d195005
Mark Vallis <markvallis1987@Gmail.com>
2025-05-01 16:25:43 +0100

    Update git_stale_branch_checker.sh

--- Example Files and Notes/Bash Scripts/git_stale_branch_checker.sh
+++ Example Files and Notes/Bash Scripts/git_stale_branch_checker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Modified version of https://www.geeksforgeeks.org/how-to-clean-old-remote-git-branches/#approach-2-automated-scripts
+# modified version of https://www.geeksforgeeks.org/how-to-clean-old-remote-git-branches/#approach-2-automated-scripts
 
 # Add this file to the repo you want to check and set it as executable if not already done (chmod +x git_stale_branch_checker.sh)
 # Run using ./git_stale_branch_checker.sh
@@ -9,16 +9,17 @@
 # Branch datastax_only is stale, the most recent commit was made by mveeprojects on Mon 12 Jun 2023 18:54:01 BST
 # Branch master is stale, the most recent commit was made by mveeprojects on Mon 28 Feb 2022 14:45:48 GMT
 
-git fetch --prune
+# Replaced git fetch with the below based on https://stackoverflow.com/a/10312587/3059314
+# Ensures all remote branches are tracked locally, required in order to get the last commit information.
+for remote in `git branch -r`; do git branch --track ${remote#origin/} $remote; done
 
 # Get the current date minus 30 days
 THRESHOLD_DATE=$(date -j -v-30d +%s)
 
-echo Threshold date: $(date -r $THRESHOLD_DATE)
+echo "Threshold date: $(date -r $THRESHOLD_DATE)"
 
 # Loop through remote branches
 for branch in $(git branch -r | grep -v '\->'); do
-    # Get the last commit date of the branch
     LAST_COMMIT_DATE=$(git log -1 --format=%ct "${branch#origin/}")
     LAST_COMMIT_AUTHOR=$(git log -1 --pretty=format:%an "${branch#origin/}")
 
