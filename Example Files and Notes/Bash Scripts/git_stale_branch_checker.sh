4a1cec5e08f9d854eb15a6af98e2acc7fe4aa1d8
Mark Vallis <markvallis1987@Gmail.com>
2025-05-02 09:19:15 +0100

    Update git_stale_branch_checker.sh

--- Example Files and Notes/Bash Scripts/git_stale_branch_checker.sh
+++ Example Files and Notes/Bash Scripts/git_stale_branch_checker.sh
@@ -4,11 +4,6 @@
 # Add this file to the repo you want to check and set it as executable if not already done (chmod +x git_stale_branch_checker.sh)
 # Run using ./git_stale_branch_checker.sh
 
-# Example output:
-# Threshold date: Tue 1 Apr 2025 15:24:35 BST
-# Branch datastax_only is stale, the most recent commit was made by mveeprojects on Mon 12 Jun 2023 18:54:01 BST
-# Branch master is stale, the most recent commit was made by mveeprojects on Mon 28 Feb 2022 14:45:48 GMT
-
 # Replaced git fetch with the below based on https://stackoverflow.com/a/10312587/3059314
 # Ensures all remote branches are tracked locally, required in order to get the last commit information.
 for remote in `git branch -r`; do git branch --track ${remote#origin/} $remote; done
